7a7aa58621ce629eb7251f333580aef8b5966527
hvt <truongghoangg12@gmail.com>
2022-06-15 15:37:56 +0700

    Sửa Lỗi Và Hoàn Thiện Phần Header #2

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -52,7 +52,8 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
                                 }}
                             />
                         )}
-                        {renderItems()}
+
+                        <div className={cx('menu-body')}>{renderItems()}</div>
                     </PopperWrapper>
                 </div>
             )}
